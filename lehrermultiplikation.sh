1360aaf72e78f1696b407b15cc913842a4ca6483
Christian Berg <cberg@stud.hs-heilbronn.de>
2019-05-09 21:45:00 +0200

    Testet automatisch gleiche Zahlen von 20 bis 99

--- lehrermultiplikation.sh
+++ lehrermultiplikation.sh
@@ -1,22 +1,27 @@
 echo "Lehrer-Multiplikation ab 20"
 echo "---------------------------"
-echo "Alpha-Version:" 
+echo "Alpha-Version+ (testet automatisch):" 
 echo "- Nur gleiche Zahlen"
 echo "- Nur Zahlen mit den gleichen Zehnerstellen"
-echo "- Nur Zahlen bis 100"
+echo "- Nur Zahlen bis inklusive 99"
 echo "---------------------------"
 echo "Geschrieben am 09.05.2019 von derberg (matrix.org-Nickname)"
 
+for i in {20..99}
+do
+
+((c++))
 #0. Schritt
-read -p "Zahl eingeben: "  zahl
+zahl=$i
+
 zahl_zehner="${zahl:0:1}" #variable:offset:länge
 zahl_einer="${zahl:1:1}"
-echo "1. Test" $zahl "*" $zahl
+echo $c". Test" $zahl "*" $zahl
 
 #1. Schritt
 zahl_links=$(expr $zahl + $zahl_einer)
 zahl_rechts=$(expr $zahl_einer \* $zahl_einer) 
-echo "1.1" $zahl_links $zahl_rechts
+echo $c".1" $zahl_links $zahl_rechts
 
 #2. Schritt
 if [[ $(expr length $zahl_rechts) = "1" ]]; then #Nur eine Stelle?
@@ -24,11 +29,11 @@ if [[ $(expr length $zahl_rechts) = "1" ]]; then #Nur eine Stelle?
 fi
 zahl_kombiniert=$(expr $zahl_links + "${zahl_rechts:0:1}")
 zahl_kombiniert=$zahl_kombiniert"${zahl_rechts:1:1}" #Zeichen hinzufügen
-echo "1.2" $zahl_kombiniert
+echo $c".2" $zahl_kombiniert
 
 #3. Schritt
 zahl_malzehner=$(expr $zahl_kombiniert \* $zahl_zehner) 
-echo "1.3" $zahl_kombiniert"*"$zahl_zehner"="$zahl_malzehner
+echo $c".3" $zahl_kombiniert"*"$zahl_zehner"="$zahl_malzehner
 
 ##4. Schritt
 #zahl_ohne_einerabweichung=$(expr $zahl_malzehner - $zahl_rechts)
@@ -38,12 +43,15 @@ echo "1.3" $zahl_kombiniert"*"$zahl_zehner"="$zahl_malzehner
 zahl_zehner_minus1=$(expr $zahl_zehner - 1)
 zahl_abweichung=$(expr $zahl_rechts \* $zahl_zehner_minus1)
 zahl_endergebnis=$(expr $zahl_malzehner - $zahl_abweichung)
-echo $zahl_endergebnis "(Erwartet:" $(expr $zahl \* $zahl)")"
+echo $c".4" $zahl_endergebnis "(Erwartet:" $(expr $zahl \* $zahl)")"
 
 #5. Schritt
 if [[ $zahl_endergebnis = $(expr $zahl \* $zahl) ]]; then #Stimmt das Endergebnis?
- echo "Stimmt!"
+ echo $c".5 Stimmt!"
 else
- echo "Stimmt nicht!"
+ echo $c".5 Stimmt nicht!"
  echo $zahl >> error.txt
 fi
+ echo " "
+ 
+done
