334a8ff219fe6a38d944ccff89af821ce8155013
Christian Berg <cberg@stud.hs-heilbronn.de>
2019-05-25 14:47:59 +0200

    Gleiche Zehner

--- lehrermultiplikation.sh
+++ lehrermultiplikation.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Lehrer-Multiplikation ab 20"
 echo "---------------------------"
-echo "Alpha-Version++ (mit Optionen): "
+echo "Beta-Version++ (mit Optionen): "
 echo " - Nur gleiche Zahlen"
 echo " - Nur Zahlen mit den gleichen Zehnerstellen"
 echo " - Nur Zahlen bis inklusive 99"
 echo "---------------------------"
+echo "Links und rechts gleiche Zahl"
+echo "./lehrermultiplikation.sh  gleich"
 echo " Testen von  Zahlen innerhalb eines Bereiches:"
 echo "./lehrermultiplikation.sh  31 36"
 echo "Manuelle Eingabe:"
@@ -30,11 +32,70 @@ else
  then
   read -p "Zahl eingeben: " start
   ende=$start
- else
+  #Fragen ob gleiche Zahlen oder jede Zahl
+  #Abfragen der Range
+  #Vielleicht auch jede x.Zahl oder jede Zahl endend auf x, enthaltend x oder beginnend mit x..
+ elif [ "$1" = "gleich" ]
+ then
   if [ "$1" = "" ]; then start=20; else start=$1; fi
   if [ "$2" = "" ]; then ende=99; else ende=$2; fi
+ else
+  #Hmmm
+    read -p "Linke Zahl eingeben: " links
+    read -p "Rechte Zahl eingeben: " rechts
+    start=1
+    ende=0
  fi
 
+ 
+((c++))
+#Zwei Zahlen
+#0. Schritt
+links_zehner="${links:0:1}" #variable:offset:länge
+links_einer="${links:1:1}"
+rechts_zehner="${rechts:0:1}" #variable:offset:länge
+rechts_einer="${rechts:1:1}"
+echo $c". Test" $links "*" $rechts
+
+#1. Schritt
+zahl_links=$(expr $links + $rechts_einer)
+zahl_rechts=$(expr $links_einer \* $rechts_einer) 
+echo $c".1" $zahl_links $zahl_rechts
+ 
+#2. Schritt
+if [[ $(expr length $zahl_rechts) = "1" ]]; then #Nur eine Stelle?
+ zahl_rechts=0$zahl_rechts #Man könnte auch immer 0en hinzufügen und dann von hinten gehen..
+fi
+zahl_kombiniert=$(expr $zahl_links + "${zahl_rechts:0:1}")
+zahl_kombiniert=$zahl_kombiniert"${zahl_rechts:1:1}" #Zeichen hinzufügen
+echo $c".2" $zahl_kombiniert
+ 
+#3. Schritt
+zahl_malzehner=$(expr $zahl_kombiniert \* $links_zehner) #links_zehner und rechts_zehner sind allerdings wohl auch gleich
+echo $c".3" $zahl_kombiniert"*"$links_zehner"="$zahl_malzehner
+ 
+##4. Schritt
+#zahl_ohne_einerabweichung=$(expr $zahl_malzehner - $zahl_rechts)
+#echo $zahl_ohne_einerabweichung "(Erwartet:" $(expr $zahl \* $zahl)")"
+
+#4. Schritt
+zahl_zehner_minus1=$(expr $links_zehner - 1)
+zahl_abweichung=$(expr $zahl_rechts \* $zahl_zehner_minus1)
+zahl_endergebnis=$(expr $zahl_malzehner - $zahl_abweichung)
+echo $c".4" $zahl_malzehner"-"$zahl_zehner_minus1"*"$zahl_rechts"="$zahl_endergebnis "(Erwartet:" $(expr $links \* $rechts)")"
+
+#5. Schritt
+if [[ $zahl_endergebnis = $(expr $links \* $rechts) ]]; then #Stimmt das Endergebnis?
+ echo $c".5 Stimmt!"
+else
+ echo $c".5 Stimmt nicht!"
+ echo $zahl >> error.txt
+fi
+ echo " "
+ 
+ 
+ 
+#Eine Zahl 
 for (( i=$start; i<=$ende; i++ ))
 do
 
